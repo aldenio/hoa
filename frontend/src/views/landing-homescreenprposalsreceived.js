fa9399441a616c4cee1e1f6baa3a0b16abf3ac2d
aldenio <aldenio@gmail.com>
2024-10-20 08:52:21 -0700

    Integrated RepoB into RepoA

---
+++ frontend/src/views/landing-homescreenprposalsreceived.js
@@ -0,0 +1,315 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './landing-homescreenprposalsreceived.css'
+
+const LandingHomescreenprposalsreceived = (props) => {
+  return (
+    <div className="landing-homescreenprposalsreceived-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="landing-homescreenprposalsreceived-landing-homescreenprposalsreceived">
+        <div className="landing-homescreenprposalsreceived-frame25">
+          <div className="landing-homescreenprposalsreceived-group9">
+            <div className="landing-homescreenprposalsreceived-frame20">
+              <div className="landing-homescreenprposalsreceived-status-bari-phone14">
+                <div className="landing-homescreenprposalsreceived-left-side">
+                  <div className="landing-homescreenprposalsreceived-frame-status-bartime">
+                    <span className="landing-homescreenprposalsreceived-text10 DefaultBoldBody">
+                      <span>9:41</span>
+                    </span>
+                  </div>
+                </div>
+                <div className="landing-homescreenprposalsreceived-right-side">
+                  <div className="landing-homescreenprposalsreceived-battery">
+                    <img
+                      alt="OutlineI301"
+                      src="/external/outlinei301-mem.svg"
+                      className="landing-homescreenprposalsreceived-outline"
+                    />
+                    <img
+                      alt="BatteryEndI301"
+                      src="/external/batteryendi301-hzmb.svg"
+                      className="landing-homescreenprposalsreceived-battery-end"
+                    />
+                    <img
+                      alt="FillI301"
+                      src="/external/filli301-q1bj.svg"
+                      className="landing-homescreenprposalsreceived-fill"
+                    />
+                  </div>
+                  <img
+                    alt="WifiI301"
+                    src="/external/wifii301-rcs.svg"
+                    className="landing-homescreenprposalsreceived-wifi"
+                  />
+                  <img
+                    alt="IconMobileSignalI301"
+                    src="/external/iconmobilesignali301-qp.svg"
+                    className="landing-homescreenprposalsreceived-icon-mobile-signal"
+                  />
+                </div>
+              </div>
+              <div className="landing-homescreenprposalsreceived-avatar-block1">
+                <div className="landing-homescreenprposalsreceived-avatar1">
+                  <img
+                    alt="ShapeI301"
+                    src="/external/shapei301-jtk-200w.png"
+                    className="landing-homescreenprposalsreceived-shape1"
+                  />
+                </div>
+                <div className="landing-homescreenprposalsreceived-info1">
+                  <span className="landing-homescreenprposalsreceived-text12 BodyStrong">
+                    <span>Sara Menon</span>
+                  </span>
+                  <span className="landing-homescreenprposalsreceived-text14 BodyBase">
+                    <span>Housing Association member</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <button className="landing-homescreenprposalsreceived-button">
+              <img
+                alt="Edit3I301"
+                src="/external/edit3i301-w62.svg"
+                className="landing-homescreenprposalsreceived-edit3"
+              />
+              <span className="landing-homescreenprposalsreceived-text16 SingleLineBodyBase">
+                <span>
+                  Create new project proposal
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+            </button>
+            <div className="landing-homescreenprposalsreceived-menu-separator">
+              <img
+                alt="RuleI301"
+                src="/external/rulei301-ia2e-200h.png"
+                className="landing-homescreenprposalsreceived-rule"
+              />
+            </div>
+          </div>
+          <div className="landing-homescreenprposalsreceived-frame24">
+            <div className="landing-homescreenprposalsreceived-tabs">
+              <div className="landing-homescreenprposalsreceived-tab1">
+                <span className="landing-homescreenprposalsreceived-text18 BodyBase">
+                  <span>Open</span>
+                </span>
+              </div>
+              <div className="landing-homescreenprposalsreceived-tab2">
+                <span className="landing-homescreenprposalsreceived-text20 BodyBase">
+                  <span>Closed</span>
+                </span>
+              </div>
+            </div>
+            <div className="landing-homescreenprposalsreceived-frame19">
+              <div className="landing-homescreenprposalsreceived-menu-item">
+                <img
+                  alt="Table3010"
+                  src="/external/table3010-khw9.svg"
+                  className="landing-homescreenprposalsreceived-table"
+                />
+                <div className="landing-homescreenprposalsreceived-body">
+                  <div className="landing-homescreenprposalsreceived-row">
+                    <span className="landing-homescreenprposalsreceived-text22 BodyBase">
+                      <span>Playground Equipment Maintenance</span>
+                    </span>
+                    <div className="landing-homescreenprposalsreceived-menu-shortcut">
+                      <span className="landing-homescreenprposalsreceived-text24 SingleLineBodySmallStrong">
+                        <span>Scheduled</span>
+                      </span>
+                    </div>
+                  </div>
+                  <span className="landing-homescreenprposalsreceived-text26 BodySmall">
+                    <span>
+                      Annual safety inspection and maintenance of playground
+                      equipment in Redwood Park.
+                    </span>
+                  </span>
+                  <div className="landing-homescreenprposalsreceived-tag">
+                    <span className="landing-homescreenprposalsreceived-text28 SingleLineBodyBase">
+                      <span>Safety</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="landing-homescreenprposalsreceived-select-field">
+                <span className="landing-homescreenprposalsreceived-text30 BodyBase">
+                  <span>Quotes from contractors</span>
+                </span>
+                <div className="landing-homescreenprposalsreceived-select">
+                  <span className="landing-homescreenprposalsreceived-text32 SingleLineBodyBase">
+                    <span>3 received</span>
+                  </span>
+                  <img
+                    alt="MinusI301"
+                    src="/external/minusi301-5lhn.svg"
+                    className="landing-homescreenprposalsreceived-minus"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="landing-homescreenprposalsreceived-group12">
+            <div className="landing-homescreenprposalsreceived-quote-card1">
+              <div className="landing-homescreenprposalsreceived-avatar-block2">
+                <div className="landing-homescreenprposalsreceived-frame121">
+                  <div className="landing-homescreenprposalsreceived-avatar2">
+                    <img
+                      alt="ShapeI301"
+                      src="/external/shapei301-3oa-200w.png"
+                      className="landing-homescreenprposalsreceived-shape2"
+                    />
+                  </div>
+                  <div className="landing-homescreenprposalsreceived-info2">
+                    <span className="landing-homescreenprposalsreceived-text34 BodyStrong">
+                      <span>AquaTech Solutions</span>
+                    </span>
+                    <span className="landing-homescreenprposalsreceived-text36 BodyBase">
+                      <span>Daly city</span>
+                    </span>
+                  </div>
+                </div>
+                <img
+                  alt="Chevrondown3013"
+                  src="/external/chevrondown3013-4h8g.svg"
+                  className="landing-homescreenprposalsreceived-chevrondown1"
+                />
+              </div>
+              <div className="landing-homescreenprposalsreceived-review-body1">
+                <div className="landing-homescreenprposalsreceived-frame122">
+                  <div className="landing-homescreenprposalsreceived-group111">
+                    <div className="landing-homescreenprposalsreceived-text-heading1">
+                      <span className="landing-homescreenprposalsreceived-text38 Heading">
+                        <span>Quote: $17,500</span>
+                      </span>
+                    </div>
+                    <div className="landing-homescreenprposalsreceived-text40">
+                      <span className="landing-homescreenprposalsreceived-text41 BodyBase">
+                        <span>Timeframe: 3 weeks</span>
+                      </span>
+                    </div>
+                  </div>
+                  <img
+                    alt="Check3012"
+                    src="/external/check3012-dw5.svg"
+                    className="landing-homescreenprposalsreceived-check"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="landing-homescreenprposalsreceived-quote-card2">
+              <div className="landing-homescreenprposalsreceived-avatar-block3">
+                <div className="landing-homescreenprposalsreceived-frame123">
+                  <div className="landing-homescreenprposalsreceived-avatar3">
+                    <img
+                      alt="ShapeI301"
+                      src="/external/shapei301-8bel-200w.png"
+                      className="landing-homescreenprposalsreceived-shape3"
+                    />
+                  </div>
+                  <div className="landing-homescreenprposalsreceived-info3">
+                    <span className="landing-homescreenprposalsreceived-text43 BodyStrong">
+                      <span>Fountain Masters Inc.</span>
+                    </span>
+                    <span className="landing-homescreenprposalsreceived-text45 BodyBase">
+                      <span>Daly city</span>
+                    </span>
+                  </div>
+                </div>
+                <img
+                  alt="Chevrondown3013"
+                  src="/external/chevrondown3013-7v4p.svg"
+                  className="landing-homescreenprposalsreceived-chevrondown2"
+                />
+              </div>
+              <div className="landing-homescreenprposalsreceived-review-body2">
+                <div className="landing-homescreenprposalsreceived-frame124">
+                  <div className="landing-homescreenprposalsreceived-group112">
+                    <div className="landing-homescreenprposalsreceived-text-heading2">
+                      <span className="landing-homescreenprposalsreceived-text47 Heading">
+                        <span>Quote: $19,200</span>
+                      </span>
+                    </div>
+                    <div className="landing-homescreenprposalsreceived-text49">
+                      <span className="landing-homescreenprposalsreceived-text50 BodyBase">
+                        <span>Timeframe: 4 weeks</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="landing-homescreenprposalsreceived-quote-card3">
+              <div className="landing-homescreenprposalsreceived-avatar-block4">
+                <div className="landing-homescreenprposalsreceived-frame125">
+                  <div className="landing-homescreenprposalsreceived-avatar4">
+                    <img
+                      alt="ShapeI301"
+                      src="/external/shapei301-ltal-200h.png"
+                      className="landing-homescreenprposalsreceived-shape4"
+                    />
+                  </div>
+                  <div className="landing-homescreenprposalsreceived-info4">
+                    <span className="landing-homescreenprposalsreceived-text52 BodyStrong">
+                      <span>GreenScape Renovations</span>
+                    </span>
+                    <span className="landing-homescreenprposalsreceived-text54 BodyBase">
+                      <span>Daly city</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="landing-homescreenprposalsreceived-review-body3">
+                <div className="landing-homescreenprposalsreceived-frame126">
+                  <div className="landing-homescreenprposalsreceived-group113">
+                    <div className="landing-homescreenprposalsreceived-text-heading3">
+                      <span className="landing-homescreenprposalsreceived-text56 Heading">
+                        <span>Quote: $16,800</span>
+                      </span>
+                    </div>
+                    <div className="landing-homescreenprposalsreceived-text58">
+                      <span className="landing-homescreenprposalsreceived-text59 BodyBase">
+                        <span>Timeframe: 3.5 weeks</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className="landing-homescreenprposalsreceived-navigation-button-list">
+          <button className="landing-homescreenprposalsreceived-navigation-button1">
+            <img
+              alt="ActivityI391"
+              src="/external/activityi391-h4a6.svg"
+              className="landing-homescreenprposalsreceived-activity"
+            />
+            <span className="landing-homescreenprposalsreceived-text61 SingleLineBodySmallStrong">
+              <span>Proposals</span>
+            </span>
+          </button>
+          <button className="landing-homescreenprposalsreceived-navigation-button2">
+            <img
+              alt="StarI391"
+              src="/external/stari391-dq5b.svg"
+              className="landing-homescreenprposalsreceived-star"
+            />
+            <span className="landing-homescreenprposalsreceived-text63 SingleLineBodySmallStrong">
+              <span>Projects</span>
+            </span>
+          </button>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LandingHomescreenprposalsreceived
